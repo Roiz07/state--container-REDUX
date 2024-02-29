78ee1f3fcd3ca120dac143089213a5a97dfda9f9
Roiz07 <56198604+Roiz07@users.noreply.github.com>
2024-02-28 23:56:53 -0500

    Fix the issues

--- context/m.js
+++ context/m.js
@@ -6,3 +6,5 @@ document.body.innerHTML = '<div id="app"></div>';
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
 root.render(<h1>Hello, world</h1>);
+
+// fix the issues..........
